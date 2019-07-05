0c03830381c55566b2fdefcbab6f3fd2a2e45874
oumaima <ounioumaima111@gmail.com>
2019-07-05 22:19:38 +0200

    update

--- server/server.js
+++ server/server.js
@@ -5,30 +5,25 @@ const path = require('path');
 const cors = require('cors');
 const passport = require('passport');
 
-// Initialize the app
+
 const app = express();
 
-// Middlewares
-// Form Data Middleware
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// Json Body Middleware
 app.use(bodyParser.json());
 
-// Cors Middleware
+
 app.use(cors());
 
 // Seting up the static directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the passport Middleware
 app.use(passport.initialize());
-// Bring in the Passport Strategy
+
 require('./config/passport')(passport);
 
-// Bring in the Database Config and connect with the database
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db, {
     useNewUrlParser: true
@@ -41,7 +36,7 @@ mongoose.connect(db, {
 // app.get('/', (req, res) => {
 //     return res.send("<h1>Hello World</h1>");
 // });
-// Bring in the Users route
+
 const users = require('./routes/api/users');
 app.use('/api/users', users);
 
